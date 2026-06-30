c6ea0a9436e1735a0c2bf435c94f630d1474cdc8
DiegoFjord <138102450+DiegoFjord@users.noreply.github.com>
2026-06-30 01:18:18 -0700

    add more design

--- designer.py
+++ designer.py
@@ -19,12 +19,12 @@ class designer:
     def getbystring(self, nntype):
         cs = None
         match nntype:
-            case "Start": cs = canvasLin(self.canvas)
+            case "Start": cs = canvasCustom(self.canvas)
             case "Batch": cs = canvasBatch(self.canvas)
             case "Embeddings": cs = canvasEmbs(self.canvas)
-            case "Linear": cs = canvasStart(self.canvas)
+            case "Linear": cs = canvasLin(self.canvas)
             case "Script": cs = canvasNone(self.canvas)
-            case "Custom": cs = canvasNone(self.canvas)
+            case "Custom": cs = canvasCustom(self.canvas)
             case _:
                 cs = canvasNone(self.canvas)
                 print("designer: item not found")
@@ -41,14 +41,41 @@ class canvasHead:
         )
 
 
-class canvasLin(canvasHead):
+class canvasCustom(canvasHead):
     def __init__(self, canvas: tk.Canvas):
         canvasHead.__init__(self, canvas)
         self.shape_id_arr = []
 
         shape_id = canvas.create_rectangle(
-            2, 2, 19.6, 98, fill="orange", outline="")
+            10, 2, 90, 98, fill="ghost white", outline="")
         self.shape_id_arr.append(shape_id)
+
+        initial_points = [50, 2, 90, 2, 90, 42]
+        poly_id = canvas.create_polygon(
+            initial_points, fill="light blue", width=5.0)
+        self.shape_id_arr.append(poly_id)
+
+        # initial_points = [50, 4, 50, 42, 88, 42, 50, 4]
+        # line_id = canvas.create_line(initial_points, fill="black", width=5.0)
+
+        initial_points = [45, 0, 45, 45, 90, 45]
+        line_id = canvas.create_line(
+            initial_points, fill="light blue", width=5.0)
+
+        self.shape_id_arr.append(line_id)
+
+
+class canvasLin(canvasHead):
+    def __init__(self, canvas: tk.Canvas):
+        canvasHead.__init__(self, canvas)
+        self.shape_id_arr = []
+
+        for b in range(0, 5):
+            x = 41.2
+            y = b * 19.6 + 2
+            shape_id = canvas.create_oval(
+                x, y, x+17.6, y+17.6, fill="spring green2", outline="")
+            self.shape_id_arr.append(shape_id)
 
 
 class canvasEmbs(canvasHead):
@@ -75,10 +102,10 @@ class canvasBatch(canvasHead):
         self.shape_id_arr = []
 
         for a in range(0, 4):
-            x = 5
-            y = a * 22.5 + 3
+            x = 4
+            y = a * 24 + 4
             shape_id = canvas.create_rectangle(
-                x, y + 5, x+90, y+22, fill="dark blue", outline=""
+                x, y, x+92, y+20, fill="dark blue", outline=""
             )
             self.shape_id_arr.append(shape_id)
 
